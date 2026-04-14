74d4268d9be703f41c9f8db92c90477893c0ca10
PrathmeshMankar23 <Prathmeshmankar1023@gmail.com>
2026-04-15 00:16:00 +0530

    added the projects

--- components/common/Projects.js
+++ components/common/Projects.js
@@ -40,6 +40,146 @@ const Projects = () => {
       scope: "BIM - LOD 350, Modeling + Documentation, 4D Monitoring and Controls",
       status: "completed",
       type: "Commercial"
+    },
+    {
+      title: "Webworks Data Centre Project ",
+      description: "Tracking and monitoring of project using BIM 4D, Synchro and Primavera P6 in weekly frequency - tradewise/phasewise and contractor wise ",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "8,00,000 sqft",
+      location: "Navi Mumbai, India",
+      scope: "BIM - LOD 350, Modeling + Documentation, 4D Monitoring and Controls",
+      status: "Ongoing",
+      type: "Data Centre"
+    },
+    {
+      title: "Mall Project ( Kolkata ) ",
+      description: "BIM support for Edifice Client's interior works in Brookfield Mall, a high-end mall project.",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "70,000 sqft",
+      location: "Kolkata, India",
+      scope: "BIM ID - LOD 350, Modeling + Clash Detection + Clash Resolution, Support + Documentation",
+      status: "Completed",
+      type: "Retail"
+    },
+    {
+      title: "Antariksh Logistics Park, Bhiwandi",
+      description: "MEPF planning ,design and engineering support for logistic park by Space Deck Logistics  ",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "2,68,000 sqft",
+      location: "Bhiwandi, India",
+      scope: "End to End Design and drawings for the Project including DBR Preparation, Individual trade drawings preparations, SIte visit and commisioning support",
+      status: "Completed",
+      type: "Logistics"
+    },
+    {
+      title: "Mall Project ( Vizag) ",
+      description: "BIM Support for Edifice for interior trade in a high end mall project  ",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "3,30,000 sqft",
+      location: "Vizag, India",
+      scope: "BIM ID - LOD 350, Modeling + Clash Detection + Clash Resolution, Support + Documentation",
+      status: "Completed",
+      type: "Retail"
+    },
+    {
+      title: "Peer Review & Optimisation project",
+      description: "Peer review of MEPF design for luxury villas in Dubai, undertaken for Gulf Islamic Investment.",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "1,80,000 Sqft",
+      location: "Dubai, UAE",
+      scope: "Peer review adhering to norms and optimisation of design for value engineering ",
+      status: "Completed",
+      type: "Residential"
+    },
+    {
+      title: "Mission Critical Project (Confidential)",
+      description: "Rectification of architectural BIM model and sheet extraction for data center project for Edifice ( Aconnex Project) ",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "5,00,000 sqft",
+      location: "Navi Mumbai, India",
+      scope: "BIM - LOD 350, Modeling( Rectification) + Documentation",
+      status: "Ongoing",
+      type: "Data Centre"
+    },
+    {
+      title: "3 Star Hotel Project in Puri ",
+      description: "End-to-End BIM Support for a 3-star hotel project, aimed at adding value to execution by ensuring a clash-free building model for D&GM.",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "1,00,000 sqft",
+      location: "Puri, India",
+      scope: "BIM LOD 350 - All Trades ( Ar/ St/ MEPF) + Clash Detection + Clash Resolution Support + BEP Prep and Monitoring ",
+      status: "Completed",
+      type: "Hospitality"
+    },
+    {
+      title: "Mall Project ( Vizag) ",
+      description: "BIM Support for Edifice for interior trade in a high end mall project  ",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "3,30,000 sqft",
+      location: "Vizag, India",
+      scope: "BIM ID - LOD 350, Modeling + Clash Detection + Clash Resolution, Support + Documentation",
+      status: "Completed",
+      type: "Retail"
+    },
+    {
+      title: "Mission Critical Project",
+      description: "Information management services for Colt's data center project (Phases 2 & 3), provided on behalf of the general contractor.",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "3,30,000 sqft",
+      location: "Navi Mumbai, India",
+      scope: "BIM-LOD 400,500 + Clash Detection + Clash Resolution,Support + Documentation + As-Built preparation",
+      status: "Ongoing",
+      type: "Data Centre"
+    },
+    {
+      title: "Billionaire Bunglow in Goa ( Dreamsmith)",
+      description: "End to End BIM Support for Luxury Villa to add value for Execution by making the building clash free",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "30,000 sqft",
+      location: "Goa, India",
+      scope: "BIM LOD 350 - All Trades ( Ar/ St/ MEPF) + Clash Detection + Clash Resolution Support",
+      status: "Completed",
+      type: "Residential"
+    },
+    {
+      title: "Medical College Project ",
+      description: "Preperation of MEPF BOQ for Medical College and hospital at IIT Kanpur for Hosmac",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "8,00,000 sqft",
+      location: "Kanpur, India",
+      scope: "MEP BOQ - Tradewise and preparation of measurement sheet",
+      status: "Completed",
+      type: "Hospital"
+    },
+    {
+      title: "The Address Hotel and Branded Residences Development",
+      description: "Documentation support for Claramont's architecture and interior trade in a luxury 5-star hotel project.",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "36,000 sqft",
+      location: "Al Marjan, UAE",
+      scope: "BIM - LOD 350, Modeling + Documentation",
+      status: "Completed",
+      type: "Mixed Use"
+    },
+    {
+      title: "Commercial Project Visualisation",
+      description: "4D sequencing and visualization for a USA-based retrofitting project, achieved by integrating the Revit model with the MSP schedule.",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "3,30,000 sqft",
+      location: "USA",
+      scope: "4D visualization video( as planned) by Integrating BIM ( Revit ) and MS Project Schedule with LOI 350",
+      status: "Completed",
+      type: "Institutional"
+    },
+    {
+      title: "Mission Critical Project (Confidential)",
+      description: "Preperation of architectural BIM for data center project",
+      images: ["/images/project2.jpg", "/images/project2.jpg", "/images/project2.jpg"],
+      area: "4,70,000 sqft",
+      location: "USA",
+      scope: "BIM - LOD 350, Modeling + Clash Detection + Clash Resolution, Support + Documentation",
+      status: "Ongoing",
+      type: "Hyderabad, India"
     }
   ]
 
